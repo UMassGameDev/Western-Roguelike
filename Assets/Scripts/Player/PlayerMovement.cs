625d9af46f3072b64ed116f26265088b5624308c
Nj3125 <nicholas_johnson5@student.uml.edu>
2025-10-06 01:25:56 -0400

    Initial Commit

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Anything in [] is just something displayed in the editor.
+    [SerializeField, Tooltip("Distance moved per frame in meters per second")]
+    private float moveSpeed = 5f;
+
+    private Rigidbody2D playerRB;
+    private Vector3 moveDir;
+
+    // Gets the <playerRB>:
+    void Awake()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Updates <moveDir> based on what arrow button is held, and rotates sprite accordingly:
+    void Update()
+    {
+        // Update used to handle movement, but that has been delegated to FixedUpdate().
+        //-Non-Rigidbody2D-movement-----------------------
+        // float moveSpeedDt = moveSpeed * Time.deltaTime;
+        // Vector3 pos = transform.position;
+        moveDir = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.UpArrow)) { moveDir.y += 1; }
+        else if (Input.GetKey(KeyCode.LeftArrow)) { moveDir.x -= 1; }
+        else if (Input.GetKey(KeyCode.DownArrow)) { moveDir.y -= 1; }
+        else if (Input.GetKey(KeyCode.RightArrow)) { moveDir.x += 1; }
+
+        //-Non-Rigidbody2D-movement-----------------------
+        // pos += moveDir.normalized * moveSpeedDt;
+        // transform.position = pos;
+
+        bool playerIsMoving = moveDir.sqrMagnitude > 0.01f;
+        if (playerIsMoving)
+        {
+            float angle = CalculateAngle(moveDir);
+            transform.rotation = RotateBy(angle);
+        }
+    }
+
+    // Fixed update is just Update() for physics interactions.
+    // Moves the sprite by <moveSpeed> * <dt> every frame:
+    void FixedUpdate()
+    {
+        float moveSpeedDt = moveSpeed * Time.fixedDeltaTime;
+        playerRB.MovePosition(playerRB.position + (Vector2)(moveDir * moveSpeedDt));
+    }
+
+    [System.Obsolete("Obsolete if using multiple direction sprites for player.")]
+    // Returns the angle made between <moveDir> and the positive x-axis, in degrees:
+    float CalculateAngle(Vector3 moveDir)
+    {
+        float angleInRad = Mathf.Atan2(moveDir.y, moveDir.x);
+        return angleInRad * Mathf.Rad2Deg;
+    }
+
+    [System.Obsolete("Obsolete if using multiple direction sprites for player.")]
+    // Returns a quaternion representing a rotation of <angle> degrees:
+    Quaternion RotateBy(float angle)
+    {
+        // (Note, depending on direction your sprite faces you may need to +/= 90).
+        return Quaternion.Euler(0f, 0f, angle + 90f);  
+    }
+}
