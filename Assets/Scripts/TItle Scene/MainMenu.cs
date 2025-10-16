69bdcdf2dbe726cc6518c589cde571d6bcf2c273
Nj3125 <nicholas_johnson5@student.uml.edu>
2025-10-16 04:07:19 -0400

    Created simple main menu

---
+++ Assets/Scripts/TItle Scene/MainMenu.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneLoader.Instance.LoadSceneWithFade("MainScene");
+    }
+
+    public void Quit()
+    {
+        // This is so quitting is the same in the editor as it is in the build:
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
