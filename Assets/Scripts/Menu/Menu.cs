39ee6c50b164d9e66581d7d3ee17da0d733a8c2f
Nj3125 <nicholas_johnson5@student.uml.edu>
2025-10-17 05:15:58 -0400

    Added XML documentation for Menu.cs

--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -1,14 +1,37 @@
 using UnityEngine;
-using UnityEngine;
 using UnityEngine.SceneManagement;
+
+/// <summary>
+///     Class that stores all menu functionality.
+/// </summary>
 public class Menu : MonoBehaviour
 {
-    public void PlayGame()
+    /// <summary>
+    ///     Starts the game w/fade effect.
+    /// </summary>
+    /// <remarks>
+    ///     Currently, this just loads MainScene.unity.
+    ///     Update when save files are implemented.
+    /// </remarks>
+    /// <example>
+    ///     Menu.PlayGame();
+    /// </example>
+    /// <seealso cref="SceneLoader.LoadSceneWithFade(string)"/>
+    /// <seealso cref="Menu.Quit()"/>
+    public static void PlayGame()
     {
         SceneLoader.Instance.LoadSceneWithFade("MainScene");
     }
 
-    public void Quit()
+    /// <summary>
+    ///     Quits the application.
+    /// </summary>
+    /// <remarks>
+    ///     Quits to desktop, not main menu. 
+    ///     Works both in the editor as well as in the build.
+    /// </remarks>
+    /// <seealso cref="Menu.PlayGame()"/> 
+    public static void Quit()
     {
         // This is so quitting is the same in the editor as it is in the build:
         #if UNITY_EDITOR
