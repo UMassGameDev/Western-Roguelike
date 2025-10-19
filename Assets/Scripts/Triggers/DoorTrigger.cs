4c75ac8e194c04b7908182ccc33d5fa4147f763d
Nj3125 <nicholas_johnson5@student.uml.edu>
2025-10-18 21:07:54 -0400

    Added transition sounds for the doors in MainScene

--- Assets/Scripts/Triggers/DoorTrigger.cs
+++ Assets/Scripts/Triggers/DoorTrigger.cs
@@ -4,14 +4,18 @@ using UnityEngine.SceneManagement;
 public class DoorTrigger : MonoBehaviour
 {
     // This [] is an attribute, attributes mostly provide additional info in the editor.
-    [SerializeField, Tooltip("Scene this door loads into. Default = \"MainScene\"")] 
+    [SerializeField, Tooltip("Scene this door loads into. Default = \"MainScene\"")]
     private string targetScene = "MainScene";
+    
+    [SerializeField, Tooltip("The sound that plays when you go through this door.")] 
+    private AudioSource audio;
 
     // Detects if <other> is player, and if so, loads <targetScene> with a fade effect:
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            audio.Play();
             SceneLoader.Instance.LoadSceneWithFade(targetScene);
         }
     }
