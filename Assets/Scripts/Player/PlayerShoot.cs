f9b9de5ad2b9231e5aaa7a384c909719bd1e8cd8
Nj3125 <nicholas_johnson5@student.uml.edu>
2025-10-24 22:31:39 -0400

    Added shoot sound

--- Assets/Scripts/Player/PlayerShoot.cs
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -2,8 +2,17 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
-    public GameObject bulletObj;
-    public Transform muzzle;
+    [Header("Shooting")]
+    [SerializeField, Tooltip("The bullet prefab.")]
+    GameObject bulletObj;
+    [SerializeField, Tooltip("Where the bullet is spawned.")]
+    Transform muzzle;
+
+    [Header("Audio")]
+    [SerializeField, Tooltip("The sound played on Shoot().")]
+    AudioClip shootSound;
+    [SerializeField, Tooltip("Where <shootSound> is played.")]
+    AudioSource audioSource;
 
     const float cooldown = 0.5f;
     float lastShotTime;
@@ -27,9 +36,16 @@ public class PlayerShoot : MonoBehaviour
 
         GameObject bullet = Instantiate(bulletObj, muzzle.position, Quaternion.identity);
 
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        bullet.transform.rotation = Quaternion.Euler(0, 0, angle + 90);
+        float angle = CalculateAngle(direction);
+        bullet.transform.rotation = RotateBy(angle);
 
         bullet.GetComponent<Bullet>().Initialize(direction);
+
+        bool soundInitialized = audioSource != null && shootSound != null;
+        if (soundInitialized) { PlaySound(); }
     }
+
+    float CalculateAngle(Vector2 dir) => Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+    Quaternion RotateBy(float angle) => Quaternion.Euler(0, 0, angle + 90);
+    void PlaySound() => audioSource.PlayOneShot(shootSound);
 }
