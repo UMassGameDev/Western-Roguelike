f6624c128125d810d2853542ce8000e798f78083
Nj3125 <nicholas_johnson5@student.uml.edu>
2025-11-02 01:29:25 -0500

    Added documentation to CameraFollow.cs

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -1,14 +1,36 @@
+/*******************************************************
+* Script:      CameraFollow.cs
+* Author(s):   Nicholas Johnson
+* 
+* Description:
+*    This script, when attached to a camera, follows the player
+* 
+* Notes:
+*    This is a poor way to do this, use Cinemachine for player
+*    following instead, its more scalable and has other features
+*    we may want, like Camera bounding.
+*******************************************************/
 using UnityEngine;
 
+/// <summary>
+///     This component, when attached to a GameObject, will have the
+///     camera follow it. It should only be used on the player.
+/// </summary>
+/// <seealso cref="PlayerMovement">How the player moves.</seealso>
+[System.Obsolete("Switch to Cinemachine instead.")]
 public class CameraFollow : MonoBehaviour
 {
-    private Transform target;
-    public float smoothTime = 0.1f;
-    private Vector3 velocity = Vector3.zero;
+    // Editor variables:
+    [SerializeField, Tooltip("Time for Camera to catch up with player.")]
+    float smoothTime = 0.1f;
+
+    // Internal variables:
+    Transform target;  // May be better to assign this in editor.
+    Vector3 velocity = Vector3.zero;
 
     void Start()
     {
-        // Finds where the player is.
+        // Assigns playerObj to target automatically:
         GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
         if (playerObj != null)
         {
@@ -22,14 +44,17 @@ public class CameraFollow : MonoBehaviour
 
     void FixedUpdate()
     {
+        // If no target, don't do anything:
         if (target == null) return;
 
+        // Determine the targets position:
         Vector3 targetPosition = new Vector3(
             target.position.x,
             target.position.y,
             transform.position.z
         );
 
+        // Travel to that position:
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
 }
